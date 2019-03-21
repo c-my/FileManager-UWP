5b1f8cd0f53f1378cbda81ca788bf09b14b84abe
w-mj <w.mingjian@yahoo.com>
2019-03-21 16:52:40 +0800

    show file list

---
+++ FileManager_UWP/Server/FileServer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace FileManager_UWP.Server {
+    /// <summary>
+    /// 负责提供本地文件及文件夹访问服务
+    /// </summary>
+    class FileServer {
+        public async Task<IReadOnlyList<StorageFile>> getFileList() {
+            StorageFolder picturesFolder = KnownFolders.DocumentsLibrary;
+            IReadOnlyList<StorageFile> fileList = await picturesFolder.GetFilesAsync();
+            return fileList;
+        }
+    }
+}
---
+++ FileManager_UWP/View/FileListView.xaml.cs
@@ -0,0 +1,57 @@
+﻿using FileManager_UWP.Server;
+using GalaSoft.MvvmLight.Ioc;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.UI.Xaml.Controls;
+
+namespace FileManager_UWP.View {
+    public class DisplayFileFolderItem {
+        public string Name { get; set; }
+    }
+    public sealed partial class FileListView: Page {
+        ObservableCollection<DisplayFileFolderItem> Files = 
+            new ObservableCollection<DisplayFileFolderItem>();
+        public FileListView() {
+            this.InitializeComponent();
+        }
+
+        private async void GetFileList_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e) {
+            //StorageFolder picturesFolder = KnownFolders.DocumentsLibrary;
+            //IReadOnlyList<StorageFile> fileList = await picturesFolder.GetFilesAsync();
+            //foreach (StorageFile file in fileList) {
+            //    DebugPanel.Children.Add(new TextBlock { Text = file.Name });
+            //    Debug.WriteLine(file.Name);
+            //}
+
+            string path = pathText.Text;
+            StorageFolder folder = null;
+            try {
+                folder = await StorageFolder.GetFolderFromPathAsync(path);
+            } catch (Exception ex) {
+                DebugPanel.Text = ex.Message;
+            }
+            if (null != folder) {
+                TreeViewNode rootNode = new TreeViewNode() { Content = folder.Name };
+                IReadOnlyList<StorageFolder> folders = await folder.GetFoldersAsync();
+                IReadOnlyList<StorageFile> files = await folder.GetFilesAsync();
+                Files.Clear();
+                foreach(StorageFolder f in folders) {
+                    // DebugPanel.Children.Add(new TextBlock { Text = f.Name });
+                    Files.Add(new DisplayFileFolderItem { Name = f.Name });
+                }
+                foreach (StorageFile f in files) {
+                    // DebugPanel.Children.Add(new TextBlock { Text = f.Name });
+                    Files.Add(new DisplayFileFolderItem { Name = f.Name });
+                    // Files.Add(f);
+                }
+            }
+
+        }
+    }
+}
