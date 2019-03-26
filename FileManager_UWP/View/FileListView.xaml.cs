c83c9d81bd54cc842ba1af43d311aff3b1b07c19
w-mj <w.mingjian@yahoo.com>
2019-03-26 21:17:57 +0800

    click in folder

--- FileManager_UWP/View/FileListView.xaml.cs
+++ FileManager_UWP/View/FileListView.xaml.cs
@@ -13,15 +13,64 @@ using Windows.UI.Xaml.Controls;
 namespace FileManager_UWP.View {
     public class DisplayFileFolderItem {
         public string Name { get; set; }
+        public string Path { get; set; }
+        public bool IsFolder { get; set; }
+        public DisplayFileFolderItem(StorageFolder f) {
+            Name = f.Name;
+            Path = f.Path;
+            IsFolder = true;
+        }
+        public DisplayFileFolderItem(StorageFile f) {
+            Name = f.Name;
+            Path = f.Path;
+            IsFolder = false;
+        }
+        public DisplayFileFolderItem() { }
+        
+        public static DisplayFileFolderItem parent(string current) {
+            DisplayFileFolderItem d = new DisplayFileFolderItem();
+            d.Name = "..";
+            d.IsFolder = true;
+            d.Path = current.Substring(0, current.LastIndexOf('\\'));
+            if (d.Path.Length == 2)
+                d.Path += "\\";
+            return d;
+        }
     }
-    public sealed partial class FileListView: Page {
-        ObservableCollection<DisplayFileFolderItem> Files = 
+    public sealed partial class FileListView : Page {
+        ObservableCollection<DisplayFileFolderItem> Files =
             new ObservableCollection<DisplayFileFolderItem>();
         public FileListView() {
             this.InitializeComponent();
         }
 
-        private async void GetFileList_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e) {
+        private async void UpdateFileList(string path) {
+            StorageFolder folder = null;
+            try {
+                folder = await StorageFolder.GetFolderFromPathAsync(path);
+            } catch (Exception ex) {
+                DebugPanel.Text = ex.Message;
+            }
+
+            if (null != folder) {
+                TreeViewNode rootNode = new TreeViewNode() { Content = folder.Name };
+                IReadOnlyList<StorageFolder> folders = await folder.GetFoldersAsync();
+                IReadOnlyList<StorageFile> files = await folder.GetFilesAsync();
+                Files.Clear();
+                Files.Add(DisplayFileFolderItem.parent(path));
+                foreach (StorageFolder f in folders) {
+                    // DebugPanel.Children.Add(new TextBlock { Text = f.Name });
+                    Files.Add(new DisplayFileFolderItem(f));
+                }
+                foreach (StorageFile f in files) {
+                    // DebugPanel.Children.Add(new TextBlock { Text = f.Name });
+                    Files.Add(new DisplayFileFolderItem(f));
+                    // Files.Add(f);
+                }
+            }
+        }
+
+        private void GetFileList_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e) {
             //StorageFolder picturesFolder = KnownFolders.DocumentsLibrary;
             //IReadOnlyList<StorageFile> fileList = await picturesFolder.GetFilesAsync();
             //foreach (StorageFile file in fileList) {
@@ -30,28 +79,15 @@ namespace FileManager_UWP.View {
             //}
 
             string path = pathText.Text;
-            StorageFolder folder = null;
-            try {
-                folder = await StorageFolder.GetFolderFromPathAsync(path);
-            } catch (Exception ex) {
-                DebugPanel.Text = ex.Message;
-            }
-            if (null != folder) {
-                TreeViewNode rootNode = new TreeViewNode() { Content = folder.Name };
-                IReadOnlyList<StorageFolder> folders = await folder.GetFoldersAsync();
-                IReadOnlyList<StorageFile> files = await folder.GetFilesAsync();
-                Files.Clear();
-                foreach(StorageFolder f in folders) {
-                    // DebugPanel.Children.Add(new TextBlock { Text = f.Name });
-                    Files.Add(new DisplayFileFolderItem { Name = f.Name });
-                }
-                foreach (StorageFile f in files) {
-                    // DebugPanel.Children.Add(new TextBlock { Text = f.Name });
-                    Files.Add(new DisplayFileFolderItem { Name = f.Name });
-                    // Files.Add(f);
-                }
-            }
+            UpdateFileList(path);
+        }
 
+        private void ListView_ItemClick(object sender, ItemClickEventArgs e) {
+            DisplayFileFolderItem i = e.ClickedItem as DisplayFileFolderItem;
+            if (i.IsFolder) {
+                pathText.Text = i.Path;
+                UpdateFileList(i.Path);
+            }
         }
     }
 }
